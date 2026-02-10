b8ab39063c0c4a76364fb20514561a71b03de251
Sai Kumar Reddy <95733556+Sai-Kumar-Reddy-N@users.noreply.github.com>
2026-02-10 10:40:40 +0530

    Update service worker to new cache version and add activate event

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,24 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('hvac').then(c=>c.addAll(['./','index.html'])))
+const CACHE = 'hvac-v2'; // 🔴 changed version name
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(['./', 'index.html'])
+    )
+  );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
